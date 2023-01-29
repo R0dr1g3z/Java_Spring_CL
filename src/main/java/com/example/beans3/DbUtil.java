8218a5376f2457802ee9cf304452e953732c1c49
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-29 15:56:28 +0100

    change DbUtil code

--- src/main/java/com/example/beans3/DbUtil.java
+++ src/main/java/com/example/beans3/DbUtil.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-    public static String DB_URL = "jdbc:mysql://localhost:3306";
-    public static String DB_USER = "root";
-    public static String DB_PASS = "coderslab123";
-
     public static Connection connect() throws SQLException {
-        String connStr = String.format("%s/CustomerLogger?useSSL=false&characterSet=utf8mb4&serverTimezone=UTC", DB_URL);
+        final String DB_URL = "jdbc:mysql://localhost:3306";
+        final String DB_USER = "root";
+        final String DB_PASS = "coderslab123";
+        final String DATABASE = "CustomerLogger";
+        String connStr = String.format("%s/%s?useSSL=false&characterSet=utf8mb4&serverTimezone=UTC", DB_URL, DATABASE);
         Connection connection = DriverManager.getConnection(connStr, DB_USER, DB_PASS);
         return connection;
     }
